0e34f5fa3c81594f696d2388fe42b4c5008d7849
Marcelo Dias <marcelod789@gmail.com>
2023-05-10 18:42:56 -0300

    Atualização do script de criação das tabelas do Banco de Dados

--- MLTradingStocks_Docker/BD.sql
+++ MLTradingStocks_Docker/BD.sql
@@ -1,18 +1,33 @@
-CREATE DATABASE "MLTradingStocks";
+CREATE DATABASE "dados_simstock";
 
-CREATE TABLE MLTradingStocks.tbClient (
+CREATE TABLE stockData (
+    id SERIAL PRIMARY KEY,
+    file_date TIMESTAMP,
+    ticker VARCHAR(10),
+    day DATE,
+    shares INTEGER,
+    prices NUMERIC(10, 2),
+    time_hour INTEGER,
+    time_minute INTEGER,
+    time_second INTEGER,
+    last_10_prices NUMERIC(10, 2),
+    last_10_shares INTEGER,
+    is_test BOOLEAN
+);
+
+CREATE TABLE tbClient (
   idUser SERIAL PRIMARY KEY,
   login VARCHAR(255),
   hashSenha VARCHAR(255),
   email VARCHAR(255)
 );
 
-CREATE TABLE MLTradingStocks.tblCompany (
+CREATE TABLE tblCompany (
   idCompany SERIAL PRIMARY KEY,
   name VARCHAR(255)
 );
 
-CREATE TABLE MLTradingStocks.tblOffer (
+CREATE TABLE tblOffer (
   idOffer SERIAL PRIMARY KEY,
   idCompany INT,
   qtdCota INT,
@@ -22,7 +37,7 @@ CREATE TABLE MLTradingStocks.tblOffer (
   FOREIGN KEY (idCompany) REFERENCES tblCompany(idCompany)
 );
 
-CREATE TABLE MLTradingStocks.tblOfferHist (
+CREATE TABLE tblOfferHist (
   idHist SERIAL PRIMARY KEY,
   idOffer INT,
   qtdCota INT,
