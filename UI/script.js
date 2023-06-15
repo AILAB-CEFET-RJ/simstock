911f9cab5b7f790b7cb3f05874e25e353f82ea4e
Marcelo Dias <marcelo.dias@reserve.com.br>
2023-06-14 23:05:24 -0300

    Criação da funcionalidade de compra e venda das ações

--- UI/script.js
+++ UI/script.js
@@ -1,8 +1,14 @@
 const stockItemsElement = document.getElementById('stockItems');
 const buyStockForm = document.getElementById('buyStockForm');
 const sellStockForm = document.getElementById('sellStockForm');
-let saldo = 1000.00; // Saldo inicial do usuário
+let saldo = 10000.00; // Saldo inicial do usuário
 let chartLoaded;
+let grafico;
+
+const TipoOperacao = {
+  Compra: 0,
+  Venda: 1
+};
 
 // Função para adicionar uma ação na lista
 function addStockItem(symbol, quantity) {
@@ -17,7 +23,8 @@ function buyStock(event) {
     const stockSymbol = document.getElementById('stockSymbol').value;
     const stockQuantity = document.getElementById('stockQuantity').value;
     addStockItem(stockSymbol, stockQuantity);
-    comprarAcao(stockSymbol * stockQuantity);  
+    debugger;
+    comprarAcao(stockSymbol, stockQuantity);  
     buyStockForm.reset();
 }
 
@@ -27,48 +34,36 @@ function sellStock(event) {
     const sellStockSymbol = document.getElementById;
 }
 
-//Função para consumir os dados do BD
-
-// function obterDados() {
-//     $.ajax({
-//       url: 'http://localhost:3000/dados',
-//       type: 'GET',
-//       success: function(data) {
-//         exibirDados(data); // Chama a função exibirDados para mostrar os dados recebidos
-//       },
-//       error: function(xhr, status, error) {
-//         console.error('Erro na requisição:', error);
-//       }
-//     });
-//   }
-
-//   function exibirDados(data) {
-//     // Limpa o conteúdo atual da div "dados"
-//     $('#dados').empty();
-
-//     // Itera sobre os dados recebidos e os exibe na div "dados"
-//     data.forEach(function(item) {
-//       var elemento = $('<p>').text(JSON.stringify(item));
-//       $('#dados').append(elemento);
-//     });
-//   }
-
-  // Chama a função obterDados quando a página é carregada
-//   $(document).ready(function() {
-//     obterDados();
-//   });
-
-function obterDadosEAtualizarGrafico() {
+function obterDadosEAtualizarGrafico(acao) {
     $.ajax({
-      url: 'http://localhost:3000/dados',
+      url: 'http://localhost:3000/dados?acao=' + acao,
+      type: 'GET',
+      success: function(data) {
+        criarGrafico(data); // Chama a função criarGrafico com os dados recebidos
+      },
+      error: function(xhr, status, error) {
+        console.error('Erro na requisição:', error);
+      }
+    });
+  }
+
+
+  function obterDadosAcoes() {
+    $.ajax({
+      url: 'http://localhost:3000/acoes',
       type: 'GET',
       success: function(data) {
         var dropdownOptions = $('#dropdownOptions'); // Seleciona o elemento do dropdown
         dropdownOptions.empty();
-        criarGrafico(data); // Chama a função criarGrafico com os dados recebidos
         data.forEach(function(item) {
-          var option = $('<a>').addClass('dropdown-item').attr('href', '#').text(item.ticker);
+          var option = $('<a>').addClass('dropdown-item').attr('href', '#').text(item.name);
           dropdownOptions.append(option);
+        });
+        $('.dropdown-item').click(function() {
+          var selectedOption = $(this).text();
+          $('#dropdownMenuButton').text(selectedOption); // Atualiza o texto do botão dropdown
+          $('#acao').text(selectedOption); // Atualiza o texto do botão dropdown
+          obterDadosEAtualizarGrafico(selectedOption);
         });
       },
       error: function(xhr, status, error) {
@@ -76,6 +71,7 @@ function obterDadosEAtualizarGrafico() {
       }
     });
   }
+
 
   function criarGrafico(data) {
     var labels = []; // Array para armazenar os rótulos no eixo x (tempo)
@@ -89,9 +85,12 @@ function obterDadosEAtualizarGrafico() {
 
     // Obtém o contexto do canvas
     var ctx = document.getElementById('grafico').getContext('2d');
+    
+    if(grafico)
+      grafico.destroy();
 
     // Cria o gráfico
-    new Chart(ctx, {
+    grafico = new Chart(ctx, {
       type: 'line',
       data: {
         labels: labels,
@@ -124,108 +123,74 @@ function obterDadosEAtualizarGrafico() {
     });
   }
 
-  // $(document).ready(function() {
-  //   obterDadosEAtualizarGrafico(); // Chama a função obterDadosEAtualizarGrafico quando a página é carregada
-
-  //   // Atualiza o gráfico a cada 2 minutos
-  //   setInterval(function() {
-  //     obterDadosEAtualizarGrafico();
-  //   }, 120000); // 2 minutos em milissegundos
-  // });
-
-
-function renderStockChart(labels, data) {
-    const ctx = document.getElementById('stockChart').getContext('2d');
-    const stockChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Valor da Ação',
-                data: data,
-                borderColor: 'rgba(75, 192, 192, 1)',
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-}
-
-// Função para obter os dados da API da Alpha Vantage
-function getStockData(period) {
-    const apiKey = 'sk_33071526cae24d11aa38d8fc0f74f5d0'; // Substitua pela sua chave de API da IEX Cloud
-    const symbol = 'AAPL'; // Substitua pelo símbolo da ação desejada
-    const baseUrl = 'https://cloud.iexapis.com/v1';
-    const endpoint = `/stock/${symbol}/chart/${period}?token=${apiKey}`;
-    const url = `${baseUrl}${endpoint}`;
-    
-    // Realize uma requisição AJAX para obter os dados da API
-    // Exemplo usando o fetch:
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // Processar os dados e extrair os valores das ações para o período desejado
-            const labels = data.map(item => item.date); // Obter as datas para o período desejado
-            const stockValues = data.map(item => item.close); // Obter os valores de fechamento das ações
-            renderStockChart(labels.reverse(), stockValues.reverse()); // Renderizar o gráfico com os dados
-        })
-        .catch(error => console.error(error));
-}
-
-function handleSearchFormSubmit(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
-    
-    // Obter o valor digitado na barra de busca
-    const searchInput = document.getElementById('searchInput');
-    const searchTerm = searchInput.value;
-    
-    // Realizar a ação de busca com o valor digitado (por exemplo, buscar as ações correspondentes)
-    // Aqui você pode implementar a lógica de busca de acordo com o seu sistema
-    
-    // Limpar o valor da barra de busca após o envio
-    searchInput.value = '';
-}
-
-// Adicionar o evento de envio do formulário de busca ao carregar a página
-document.addEventListener('DOMContentLoaded', () => {
-    const searchForm = document.getElementById('searchForm');
-    searchForm.addEventListener('submit', handleSearchFormSubmit);
-});
-
 // Função para comprar uma ação e deduzir o valor do saldo
-function comprarAcao(valorAcao) {
-    if (saldo >= valorAcao) {
-        saldo -= valorAcao; // Deduzir o valor da ação do saldo
-        alert(`Ação comprada! Saldo restante: R$ ${saldo.toFixed(2)}`);
-        // Aqui você pode implementar a lógica de compra da ação, como atualizar o saldo do usuário no banco de dados, etc.
+function comprarAcao(valorAcao, qtdAcoes) {
+    valorTotal = valorAcao * qtdAcoes;
+
+    if (saldo >= valorTotal) {
+        saldo -= valorTotal; // Deduzir o valor da ação do saldo
+
+        const data = {
+          qtdcota: qtdAcoes,
+          preco: valorAcao,
+          timestamp: new Date().toISOString(),
+          tipooffer: TipoOperacao.Compra
+        };
+
+        $.ajax({
+          url: 'http://localhost:3000/compraVenda',
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify(data),
+          success: function(result) {
+            console.log('Valores salvos com sucesso:', result);
+            alert(`Ação comprada! Saldo restante: R$ ${saldo.toFixed(2)}`);
+            atualizarSaldo();
+          },
+          error: function(error) {
+            console.error('Erro ao salvar valores:', error);
+          }
+        });
+
     } else {
         alert("Saldo insuficiente!");
-        // Aqui você pode implementar a lógica de tratamento para saldo insuficiente, como exibir uma mensagem de erro, etc.
+       
     }
 }   
 
-function venderAcao(valorAcao) {
-        saldo += valorAcao; // Deduzir o valor da ação do saldo
-        alert(`Ação vendida! Saldo restante: R$ ${saldo.toFixed(2)}`);
-        // Aqui você pode implementar a lógica de compra da ação, como atualizar o saldo do usuário no banco de dados, etc.
-    // } else {
-    //     alert("Quantidade insuficiente!");
-    //     // Aqui você pode implementar a lógica de tratamento para saldo insuficiente, como exibir uma mensagem de erro, etc.
-    // }
+function venderAcao(valorAcao, qtdAcoes) {
+        valorTotal = valorAcao * qtdAcoes;
+        saldo += valorTotal; // Deduzir o valor da ação do saldo
+
+        const data = {
+          qtdcota: qtdAcoes,
+          preco: valorAcao,
+          timestamp: new Date().toISOString(),
+          tipooffer: TipoOperacao.Venda
+        };
+
+        $.ajax({
+          url: 'http://localhost:3000/compraVenda',
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify(data),
+          success: function(result) {
+            console.log('Valores salvos com sucesso:', result);
+            alert(`Ação vendida! Saldo restante: R$ ${saldo.toFixed(2)}`);
+            atualizarSaldo();
+          },
+          error: function(error) {
+            console.error('Erro ao salvar valores:', error);
+          }
+        });
+
+
 }   
 
-getStockData();
-obterDadosEAtualizarGrafico();
+function atualizarSaldo() {
+  $('#saldo').text(`R$${saldo.toFixed(2)}`);
+}
 
+atualizarSaldo();
+obterDadosAcoes();
 
-
-// Chamada da função para obter os dados do período desejado (1 dia, 1 semana ou 1 ano
--- UI/server.js
+++ UI/server.js
@@ -6,20 +6,23 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json()); 
+
 
 // Configuração do banco de dados **********************************************
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'simstock_database',
+  database: 'dados_simstock',
   password: 'suasenha',
   port: 5432
 });
 
 // Rota para obter os dados do banco de dados ***********************************************
 app.get('/dados', (req, res) => {
+  var acao = req.query.acao;
   // Consulta os dados na tabela desejada
-  pool.query("select  * from stockdata where ticker = 'AAPL' and id between 591 and 650", (err, result) => {
+  pool.query("select * from stockdata where ticker = '" + acao + "' ORDER BY id DESC LIMIT 10;", (err, result) => {
     if (err) {
       console.error('Erro ao consultar dados:', err);
       res.status(500).send('Erro ao consultar dados');
@@ -27,6 +30,40 @@ app.get('/dados', (req, res) => {
     }
 
     res.send(result.rows); // Envia os dados como resposta da requisição
+  });
+});
+
+// Rota para obter os dados do banco de dados ***********************************************
+app.get('/acoes', (req, res) => {
+  // Consulta os dados na tabela desejada
+  pool.query("select  * from tblcompany", (err, result) => {
+    if (err) {
+      console.error('Erro ao consultar dados:', err);
+      res.status(500).send('Erro ao consultar dados');
+      return;
+    }
+
+    res.send(result.rows); // Envia os dados como resposta da requisição
+  });
+});
+
+// Rota para obter os dados do banco de dados ***********************************************
+app.post('/compraVenda', (req, res) => {
+  var qtdcota = req.body.qtdcota;
+  var preco = req.body.preco;
+  var timestamp = req.body.timestamp;
+  var tipooffer = req.body.tipooffer;
+
+  var query = `INSERT INTO public.tblofferhist(qtdcota, preco, "timestamp", tipooffer) VALUES ( ${qtdcota}, ${preco}, '${timestamp}', ${tipooffer});`;
+  // Consulta os dados na tabela desejada
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.error('Erro ao consultar dados:', err);
+      res.status(500).send('Erro ao consultar dados');
+      return;
+    }
+
+    res.sendStatus(200);
   });
 });
 
